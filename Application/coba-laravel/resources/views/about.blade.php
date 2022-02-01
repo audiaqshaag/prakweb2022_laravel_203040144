e21475f91374e6a5ba5714ff7e21d4374e43aef8
audi <audiaqsha578@gmail.com>
2022-02-02 01:09:34 +0700

    Pertemuan 5, Video 3, Struktur Folder

---
+++ Application/coba-laravel/resources/views/about.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WPU BLOG | About</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <h1>Halaman About</h1>
+
+    <h3><?php echo $name; ?> </h3>
+    <p><?php echo $email ?></p>
+    <img src="img/<?php echo $image ?>" style="height:400px; width:auto;" alt="<?php echo $name ?>">
+
+
+
+
+    <script src="js/script.js"></script>
+</body>
+
+</html>
--- Application/coba-laravel/routes/web.php
+++ Application/coba-laravel/routes/web.php
@@ -14,5 +14,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about',[
+        "name"=>"Audi Aqsha",
+        "email"=>"audiaqsha578@gmail.com",
+        "image"=>"foto.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('blog');
 });
